c1c5e6dc2f3e20a7b210e01c77d58b87e27e38c0
Uladzislau Andrashyn <andrashyn@gmail.com>
2022-05-16 02:36:57 +0300

    alllasdasd

--- src/main/java/andr/maven/andr/lec_13/homework/task_13/MobileFilter.java
+++ src/main/java/andr/maven/andr/lec_13/homework/task_13/MobileFilter.java
@@ -3,10 +3,10 @@ package andr.maven.andr.lec_13.homework.task_13;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MobileFilter {
-    public static void main(String[] args) {
-        List<MobilePrice> mobilePrices = new ArrayList<>();
-        mobilePrices.add(new MobilePrice("Apple", "13 Mini", 150));
+//public class MobileFilter {
+  //  public static void main(String[] args) {
+     //   List<MobilePrice> mobilePrices = new ArrayList<>();
+  //      mobilePrices.add(new MobilePrice("Apple", "13 Mini", 150));
 
-    }
-}
+  //  }
+//}
--- src/main/java/andr/maven/andr/lec_14/homework/task_1/lapkavach_contents.java
+++ src/main/java/andr/maven/andr/lec_14/homework/task_1/lapkavach_contents.java
@@ -1,2 +1,19 @@
-package andr.maven.andr.lec_14.homework.task_1;public class lapkavach_contents {
-}
+    package andr.maven.andr.lec_14.homework.task_1;
+
+    public class lapkavach_contents {
+        public static void main(String[] args) {
+
+            int filmName;
+            int releaseYear;
+            int releaseCountry;
+            int ganre;
+            int lasting;
+            int releaseGlobal;
+            int maxQuality;
+            int totalFilms = 10;
+            char zagolovki;
+            for ( totalFilms = 1; totalFilms < 11; totalFilms++);
+            System.out.println(totalFilms);
+            }
+
+        }
